bdab5bac0094742d3a6468f0306c62f970f36bc6
sanyuancap <sanyuancap@foxmail.com>
2014-11-26 17:08:18 +0800

    triangle

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,80 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    this->setGLProgram(GLProgramCache::getInstance()->getGLProgram(GLProgram::SHADER_NAME_POSITION_COLOR));
+        
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
+
+
+void HelloWorld::visit(Renderer *renderer, const Mat4 &transform, bool transformUpdated)
+{
+//    Layer::visit(renderer, transform, transformUpdated);
+    _command.init(_globalZOrder);
+    _command.func = CC_CALLBACK_0(HelloWorld::onDraw, this);
+    Director::getInstance()->getRenderer()->addCommand(&_command);
+}
+
+void HelloWorld::onDraw()
+{
+    auto glProgram = getGLProgram();
+    glProgram->use();
+    glProgram->setUniformsForBuiltins();
+    
+    auto size = Director::getInstance()->getWinSize();
+    float vertercies[] = {0,0,
+                        size.width, 0,
+                        size.width / 2, size.height};
+    float color[] = {0,1,0,1,
+                    1,0,0,1,
+                    0,0,1,1};
+    
+    GL::enableVertexAttribs(GL::VERTEX_ATTRIB_FLAG_POSITION | GL::VERTEX_ATTRIB_FLAG_COLOR);
+    glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 2, GL_FLOAT, GL_FALSE, 0, vertercies);
+    glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, color);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    CC_INCREMENT_GL_DRAWN_BATCHES_AND_VERTICES(1, 3);
+    CHECK_GL_ERROR_DEBUG();
+    
+}
---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,31 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+class HelloWorld : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();  
+    
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(HelloWorld);
+    
+    //openGL
+    virtual void visit(Renderer *renderer, const Mat4 &transform, bool transformUpdated) override;
+    
+    void onDraw();
+    
+private:
+    CustomCommand _command;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
